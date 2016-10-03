0934e0adf1ebda0a51746b028fcb2d7419c409ed
santi <santi.herranz@gmail.com>
2016-10-03 13:56:56 +0200

    Initial commit

---
+++ src/main/java/com/tecnocampus/QuizCampusApplication.java
@@ -0,0 +1,12 @@
+package com.tecnocampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizCampusApplication.class, args);
+	}
+}
