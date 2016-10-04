e212338c2b42b14f7b38f2e6370b6ec0c6bf0a2c
ignasiargemipuig <ignasiargemi@gmail.com>
2016-10-04 11:51:34 +0200

    Creació dels mètodes en les classes manager

--- src/main/java/com/tecnocampus/managers/EnquestaManager.java
+++ src/main/java/com/tecnocampus/managers/EnquestaManager.java
@@ -1,7 +1,31 @@
 package com.tecnocampus.managers;
 
+import com.tecnocampus.domain.Enquesta;
+
 /**
  * Created by ignasiargemipuig on 4/10/16.
  */
 public class EnquestaManager {
+
+    public boolean existeix(String titol) {
+        return false;
+    }
+
+    public Enquesta crearEnquesta(String titol) {
+        return null;
+    }
+
+    public void llistarEnquestes() {
+
+    }
+
+    public void llistarPreguntes(Enquesta enquesta) {
+
+    }
+
+    public Enquesta obtenirEnquesta(Enquesta enquesta) {
+        return null;
+    }
+
+
 }
--- src/main/java/com/tecnocampus/managers/RespostaManager.java
+++ src/main/java/com/tecnocampus/managers/RespostaManager.java
@@ -1,7 +1,20 @@
 package com.tecnocampus.managers;
 
+import com.tecnocampus.domain.Resposta;
+
 /**
  * Created by ignasiargemipuig on 4/10/16.
  */
 public class RespostaManager {
+    public Resposta crearResposta() {
+        return null;
+    }
+
+    public void eliminarResposta(Resposta respostaID) {
+
+    }
+
+    public void obtenirResposta(int respostaID) {
+
+    }
 }
