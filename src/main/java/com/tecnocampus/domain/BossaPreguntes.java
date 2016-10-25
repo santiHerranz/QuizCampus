cfcb36086d56d34e17fcd7a5087b1b3addb82ea2
santi <santi.herranz@gmail.com>
2016-10-25 11:01:23 +0200

    webFlow

---
+++ src/main/java/com/tecnocampus/domain/BossaPreguntes.java
@@ -0,0 +1,25 @@
+package com.tecnocampus.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by santi on 25/10/16.
+ */
+public class BossaPreguntes implements Serializable {
+        private ArrayList<Pregunta> preguntes;
+
+        public BossaPreguntes() {
+            this.preguntes = new ArrayList<>();
+        }
+
+        public void addPregunta(Pregunta note) {
+            preguntes.add(note);
+        }
+
+        public List<Pregunta> getPreguntes() {
+            return preguntes;
+        }
+    
+}
